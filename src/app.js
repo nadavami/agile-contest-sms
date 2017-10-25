f08c2e73ad0adb004479b2d0bc06e0289302b299
Nadav Ami <nadav@nadavami.com>
2017-10-25 14:01:28 -0400

    Add twilio incoming api endpoint

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 let app = express()
 
+app.use('/api/incoming', (req, res) => {
+  console.log('incoming body', req.body)
+  res.send('')
+})
+
 app.use('/', (req, res) => {
   res.send('Hello, world!')
 })
--- tests/server.test.js
+++ tests/server.test.js
@@ -18,4 +18,17 @@ describe('Test server', () => {
     let portBeforeStart = () => server.port
     expect(portBeforeStart).toThrowError('Server not started')
   })
+
+  test('Can receive external payload on /api/incoming', async () => {
+    process.env.PORT = 0
+    let server = new Server()
+    server.start()
+    let response = request({
+      uri: `http://localhost:${server.port}/api/incoming`,
+      method: 'get',
+      resolveWithFullResponse: true
+    }).then(data => data.statusCode)
+
+    await expect(response).resolves.toBe(200)
+  })
 })
